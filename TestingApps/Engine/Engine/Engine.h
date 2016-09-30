ee73bb8a53fa302099196dd3353dd479f8b16cf4
myatthu1986 <myatthu1986@gmail.com>
2016-09-30 11:31:31 +0800

    add testingapps engine dll

---
+++ TestingApps/Engine/Engine/Engine.h
@@ -0,0 +1,13 @@
+// Engine.h
+
+#pragma once
+
+using namespace System;
+
+namespace Engine {
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
---
+++ TestingApps/Engine/Engine/Main.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "Main.h"
+#include <stdio.h>
+
+
+DLL void DoWork(ProgressCallback progressCallback)
+{
+	int counter = 0;
+
+	for (; counter <= 100; counter++)
+	{
+		// do the work...
+
+		if (progressCallback)
+		{
+			// send progress update
+			progressCallback(counter);
+		}
+	}
+}
