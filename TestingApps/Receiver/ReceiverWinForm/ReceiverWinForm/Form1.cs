a2d215759a372392f9ca77c3245ccf1e86b27216
myatthu1986 <myatthu1986@gmail.com>
2016-10-03 09:10:53 +0800

    receiver in winform

--- TestingApps/Receiver/ReceiverWinForm/ReceiverWinForm/Form1.cs
+++ TestingApps/Receiver/ReceiverWinForm/ReceiverWinForm/Form1.cs
@@ -1,31 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace Receiver
+namespace ReceiverWinForm
 {
-    public class Program
+    public partial class Form1 : Form
     {
         [UnmanagedFunctionPointer(CallingConvention.StdCall)]
         delegate void CSharpCallback(int value);
 
-        //[DllImport(@"C:\Frank\SMS Project\Dummy\ServiceCallBack\TestingApps\Engine\Debug\Engine.dll", CallingConvention = CallingConvention.Cdecl)]
         [DllImport(@"C:\willowlynx\scada\arch\T-i386-ntvc\bin\DataPorting.dll", CallingConvention = CallingConvention.Cdecl)]
         static extern void DoWork_CSharpCallback([MarshalAs(UnmanagedType.FunctionPtr)] CSharpCallback callbackPointer);
 
-        static void Main(string[] args)
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
         {
             CSharpCallback callback =
                 (value) =>
                 {
-                    Console.WriteLine("rtdb value = {0}", value);
+                    MessageBox.Show("rtdb value = " + value);
                 };
 
             DoWork_CSharpCallback(callback);
-            Console.ReadKey(true);
         }
     }
 }
